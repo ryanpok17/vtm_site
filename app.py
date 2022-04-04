fa4c625d0c0075f6fb68d836c1a37ae9f90711f5
ryanpok17 <ryanm.pokrzywinski@gmail.com>
2022-04-03 20:43:28 -0500

    bug fixing

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from cmath import pi
 from flask import Flask
 from flask import render_template, redirect, request, url_for
 
@@ -7,15 +8,15 @@ friend_list = [{"name": "Mike Colbert", "email":"mike@mike.com" } ]
 
 @app.route('/')
 def index():
-    return render_template('index.html', pageTitle='Mike\'s Friends', friends = friend_list)
+    return render_template('index.html', pageTitle='Home Page')
 
-@app.route('/mike')
+@app.route('/about')
 def about():
-    return render_template('mike.html', pageTitle='About Mike')
+    return render_template('about.html', pageTitle='About VTM')
 
-@app.route('/mike')
+@app.route('/estimate')
 def estimate():
-    return render_template('mike.html', pageTitle='About Mike')
+    return render_template('estimate.html', pageTitle='estimate')
 
 @app.route('/vtm_estimate', methods=['GET', 'POST'])
 def vtm_estimate():
@@ -23,10 +24,15 @@ def vtm_estimate():
         form = request.form
         Tradius = form['Tradius']
         Theight = form['Theight']
-        #friend_dict = {"name": fname + " " + lname, "email": email}
-        #friend_list.append(friend_dict)
-        #return redirect(url_for('index'))
-    #return redirect(url_for('index'))
+        tank_top = pi * (Tradius*Tradius)
+        tank_side = 2(pi(Tradius*Theight))
+        total_area = tank_side + tank_top
+        total_sq_ft = total_area/144
+        mat_cost = total_sq_ft * 25
+        lab_cost = total_sq_ft * 15
+        cost = lab_cost + mat_cost
+        return redirect(url_for('estimate'))
+    return redirect(url_for('estimate'))
     
 
 if __name__ == '__main__':
