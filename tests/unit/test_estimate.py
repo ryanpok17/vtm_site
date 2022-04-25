110e56fb5f5a4a60f790754259d9d4c62e180368
ryanpok17 <ryanm.pokrzywinski@gmail.com>
2022-04-25 15:23:24 -0500

    debugging estimate test

--- tests/unit/test_estimate.py
+++ tests/unit/test_estimate.py
@@ -1,14 +1,6 @@
 import app
 
 def test_maintanace_cost(app, client):
-    with app.test_client() as test_client:
-        radius = 180
-        height = 360
-        res = test_client.post('/estimate', data=radius,height)
-        assert res.status_code == 302 # Found
-    
-    with app.test_client() as test_client:
-        res = test_client.get('/estimate')
-        assert res.status_code == 200
-        assert app.estimate == 141,300.00
+    assert app.estimate(radius=180,height=360) == 141,300.00
+        
     
