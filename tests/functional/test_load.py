ae258129f02b67e3b0d952f77d629514692d677c
ryanpok17 <ryanm.pokrzywinski@gmail.com>
2022-04-25 14:45:17 -0500

    Update test_load.py

--- tests/functional/test_load.py
+++ tests/functional/test_load.py
@@ -1,2 +1,17 @@
 import app
 
+def Test_home_page():
+    with app.test_client() as test_client:
+        res = test_client.get('/')
+        assert res.status_code == 200
+
+def Test_about_page():
+    with app.test_client() as test_client:
+        res = test_client.get('/about')
+        assert res.status_code == 200
+
+def Test_estimate_page():
+    with app.test_client() as test_client:
+        res = test_client.get('estimate')
+        assert res.status_code == 200
+
